d340e8ead1f92a4b87923804854d71d4394da5b5
Adrián Medve <medveadrian03@gmail.com>
2022-11-21 14:40:16 +0100

    change style and some code

--- Amoba/Form1.Designer.cs
+++ Amoba/Form1.Designer.cs
@@ -34,15 +34,16 @@ namespace Amoba
             this.tbJatekos1 = new System.Windows.Forms.TextBox();
             this.tbJatekos2 = new System.Windows.Forms.TextBox();
             this.cbKorokSzama = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbKorokSzama = new System.Windows.Forms.TextBox();
             this.btnKezdes = new System.Windows.Forms.Button();
             this.btnKilepes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblJatekos1
             // 
             this.lblJatekos1.AutoSize = true;
-            this.lblJatekos1.Location = new System.Drawing.Point(35, 34);
+            this.lblJatekos1.Location = new System.Drawing.Point(35, 81);
             this.lblJatekos1.Name = "lblJatekos1";
             this.lblJatekos1.Size = new System.Drawing.Size(115, 20);
             this.lblJatekos1.TabIndex = 6;
@@ -51,7 +52,7 @@ namespace Amoba
             // lblJatekos2
             // 
             this.lblJatekos2.AutoSize = true;
-            this.lblJatekos2.Location = new System.Drawing.Point(35, 92);
+            this.lblJatekos2.Location = new System.Drawing.Point(35, 139);
             this.lblJatekos2.Name = "lblJatekos2";
             this.lblJatekos2.Size = new System.Drawing.Size(116, 20);
             this.lblJatekos2.TabIndex = 7;
@@ -59,14 +60,14 @@ namespace Amoba
             // 
             // tbJatekos1
             // 
-            this.tbJatekos1.Location = new System.Drawing.Point(176, 31);
+            this.tbJatekos1.Location = new System.Drawing.Point(176, 78);
             this.tbJatekos1.Name = "tbJatekos1";
             this.tbJatekos1.Size = new System.Drawing.Size(187, 26);
             this.tbJatekos1.TabIndex = 0;
             // 
             // tbJatekos2
             // 
-            this.tbJatekos2.Location = new System.Drawing.Point(176, 89);
+            this.tbJatekos2.Location = new System.Drawing.Point(176, 136);
             this.tbJatekos2.Name = "tbJatekos2";
             this.tbJatekos2.Size = new System.Drawing.Size(187, 26);
             this.tbJatekos2.TabIndex = 1;
@@ -74,24 +75,25 @@ namespace Amoba
             // cbKorokSzama
             // 
             this.cbKorokSzama.AutoSize = true;
-            this.cbKorokSzama.Location = new System.Drawing.Point(115, 155);
+            this.cbKorokSzama.Location = new System.Drawing.Point(115, 202);
             this.cbKorokSzama.Name = "cbKorokSzama";
             this.cbKorokSzama.Size = new System.Drawing.Size(124, 24);
             this.cbKorokSzama.TabIndex = 2;
             this.cbKorokSzama.Text = "Körök száma:";
             this.cbKorokSzama.UseVisualStyleBackColor = true;
+            this.cbKorokSzama.CheckedChanged += new System.EventHandler(this.cbKorokSzama_CheckedChanged);
             // 
-            // textBox1
+            // tbKorokSzama
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(252, 153);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(27, 26);
-            this.textBox1.TabIndex = 3;
+            this.tbKorokSzama.Enabled = false;
+            this.tbKorokSzama.Location = new System.Drawing.Point(252, 200);
+            this.tbKorokSzama.Name = "tbKorokSzama";
+            this.tbKorokSzama.Size = new System.Drawing.Size(27, 26);
+            this.tbKorokSzama.TabIndex = 3;
             // 
             // btnKezdes
             // 
-            this.btnKezdes.Location = new System.Drawing.Point(23, 196);
+            this.btnKezdes.Location = new System.Drawing.Point(23, 243);
             this.btnKezdes.Name = "btnKezdes";
             this.btnKezdes.Size = new System.Drawing.Size(144, 45);
             this.btnKezdes.TabIndex = 4;
@@ -100,22 +102,38 @@ namespace Amoba
             // 
             // btnKilepes
             // 
-            this.btnKilepes.Location = new System.Drawing.Point(231, 196);
+            this.btnKilepes.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnKilepes.Location = new System.Drawing.Point(231, 243);
             this.btnKilepes.Name = "btnKilepes";
             this.btnKilepes.Size = new System.Drawing.Size(144, 45);
             this.btnKilepes.TabIndex = 5;
             this.btnKilepes.Text = "Kilépés";
             this.btnKilepes.UseVisualStyleBackColor = true;
+            this.btnKilepes.Click += new System.EventHandler(this.btnKilepes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(115, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "TicTacToe";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmBeallitasok
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 261);
+            this.CancelButton = this.btnKilepes;
+            this.ClientSize = new System.Drawing.Size(399, 315);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnKilepes);
             this.Controls.Add(this.btnKezdes);
             this.Controls.Add(this.cbKorokSzama);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbKorokSzama);
             this.Controls.Add(this.tbJatekos2);
             this.Controls.Add(this.tbJatekos1);
             this.Controls.Add(this.lblJatekos2);
@@ -138,9 +156,10 @@ namespace Amoba
         private System.Windows.Forms.TextBox tbJatekos1;
         private System.Windows.Forms.TextBox tbJatekos2;
         private System.Windows.Forms.CheckBox cbKorokSzama;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbKorokSzama;
         private System.Windows.Forms.Button btnKezdes;
         private System.Windows.Forms.Button btnKilepes;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Amoba/Form1.cs
+++ Amoba/Form1.cs
@@ -16,5 +16,15 @@ namespace Amoba
         {
             InitializeComponent();
         }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cbKorokSzama_CheckedChanged(object sender, EventArgs e)
+        {
+            tbKorokSzama.Enabled = cbKorokSzama.Checked;
+        }
     }
 }
